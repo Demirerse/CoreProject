6174b8440762aa11c59ac132b40aa4513fe27f31
Selim DEMIRER <slmdmrr@gmail.com>
2021-07-03 12:25:34 +0300

    DAL start

---
+++ Project.DAL/Context/MyContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Project.ENTITIES.Models;
+using Project.MAP.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.DAL.Context
+{
+    public class MyContext:IdentityDbContext<AppUser, IdentityRole<int>,int>
+    {
+        public MyContext(DbContextOptions<MyContext> options):base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new AppUserConfiguration());
+            builder.ApplyConfiguration(new ProfileConfiguration());
+            builder.ApplyConfiguration(new ArticleConfiguration());
+            builder.ApplyConfiguration(new ArticleTagConfiguration());
+            builder.ApplyConfiguration(new CategoryConfiguration());
+            builder.ApplyConfiguration(new EditorConfiguration());
+            builder.ApplyConfiguration(new TagConfiguration());
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppUserProfile> Profiles { get; set; }
+        public DbSet<ArticleTag> ArticleTags { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Editor> Editors { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+    }
+}
---
+++ Project.DAL/Repositories/Abstract/IRepository.cs
@@ -0,0 +1,42 @@
+﻿using Project.ENTITIES.CoreInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Project.DAL.Repositories.Abstract
+{
+    public interface IRepository<T> where T:class, IEntity
+    {
+        //List Commands
+        List<T> GetAll();
+        List<T> GetActives();
+        List<T> GetPassives();
+        List<T> GetModifieds();
+
+        //Modify Commands
+        void Add(T item);
+        void AddRange(List<T> list);
+        void Delete(T item);
+        void DeleteRange(List<T> list);
+        void Destroy(T item);
+        void DestroyRange(List<T> list);
+        void Update(T item);
+        void UpdateRange(List<T> list);
+
+        //Linq
+        List<T> Where(Expression<Func<T, bool>> exp);
+        bool Any(Expression<Func<T, bool>> exp);
+        T FirstOrDefault(Expression<Func<T, bool>> exp);
+        object Select(Expression<Func<T, object>> exp);
+
+        //Find Command
+        T Find(int id);
+
+        //Last Data
+        T GetLastData();
+
+        //First Data
+        T GetFirstData();
+    }
+}
